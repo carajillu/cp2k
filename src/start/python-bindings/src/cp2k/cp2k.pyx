35aef9b909e32e2152852bc119dcf62e74858cad
carajillu <joan.clark88@gmail.com>
2024-08-07 15:35:38 +0100

    reverting to last mod. Need to write class fenv

--- src/start/python-bindings/src/cp2k/cp2k.pyx
+++ src/start/python-bindings/src/cp2k/cp2k.pyx
@@ -2,5 +2,5 @@ from cpython.mem cimport PyMem_Malloc, PyMem_Free
 
 from core import initialize, finalize
 from info import version
-from fenv import fenv
-from mpi import MPI
+from mpi4py import MPI
+
